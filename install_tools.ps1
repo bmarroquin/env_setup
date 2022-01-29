d745160fa801db2fbf517f6b763d6fe005c8e9e0
Brian Marroquin <brianmarroq@gmail.com>
2022-01-29 12:46:00 -0800

    actually add reg changes

--- install_tools.ps1
+++ install_tools.ps1
@@ -1,3 +1,7 @@
 choco install notepadplusplus
 choco install 7zip
 choco install vlc
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "AllowSearchToUseLocation" -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "CortanaConsent" -Value 0
