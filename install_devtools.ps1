9ba946ad4b27faa312f022635f26789a2d940a02
Brian Marroquin <brianmarroq@gmail.com>
2022-01-29 11:14:39 -0800

    adds dev tool scripts

---
+++ install_devtools.ps1
@@ -0,0 +1,16 @@
+choco install vscode
+refreshenv
+code --install-extension rust-lang.rust
+code --install-extension ms-vscode.powershell
+code --install-extension ms-python.python
+
+choco install git --params "/NoShellIntegration /Editor:VisualStudioCode"
+choco install python3 --version=3.9.10 --params "/InstallDir:C:\ProgramData\python\python39"
+choco install visualstudio2022community
+choco install visualstudio2022-workload-nativedesktop
+choco install visualstudio2022-workload-netweb
+choco install visualstudio2022-workload-manageddesktop
+refreshenv
+curl.exe "https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe" --output rustup-init.exe
+.\rustup-init.exe --profile default -y
+rm .\rustup-init.exe
---
+++ install_tools.ps1
@@ -0,0 +1,3 @@
+choco install notepadplusplus
+choco install 7zip
+choco install vlc
